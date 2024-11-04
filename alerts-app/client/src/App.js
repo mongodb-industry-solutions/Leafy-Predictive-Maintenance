2f9cce2073825133eb92eeb6af9fd67ac98bb28a
bereilhp <bereilhp@gmail.com>
2024-11-04 14:42:56 +0100

    Cleaning up code

--- alerts-app/client/src/App.js
+++ alerts-app/client/src/App.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Modal, Button, ListGroup, Navbar } from 'react-bootstrap';
-import socketIOClient from 'socket.io-client';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Modal, Button, ListGroup, Navbar } from "react-bootstrap";
+import socketIOClient from "socket.io-client";
 
 function App() {
   const [alerts, setAlerts] = useState([]);
@@ -11,28 +10,26 @@ function App() {
 
   useEffect(() => {
     fetchAlerts();
-      // Setup WebSocket connection
-      const socket = socketIOClient("http://localhost:5003");
+    // Setup WebSocket connection
+    const socket = socketIOClient("http://localhost:5003");
 
-      // Listen for alert updates
-      socket.on('alertUpdate', (updatedAlerts) => {
-          setAlerts(prevAlerts => [...prevAlerts, updatedAlerts]);
-      });
+    // Listen for alert updates
+    socket.on("alertUpdate", (updatedAlerts) => {
+      setAlerts((prevAlerts) => [...prevAlerts, updatedAlerts]);
+    });
 
-      return () => {
-          socket.disconnect();
-      };
-
-
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
   const fetchAlerts = async () => {
     try {
-      const response = await axios.get('http://localhost:5003/api/alerts');
-      console.log(response.data)
+      const response = await axios.get("http://localhost:5003/api/alerts");
+      console.log(response.data);
       setAlerts(response.data);
     } catch (error) {
-      console.error('Error fetching alerts:', error);
+      console.error("Error fetching alerts:", error);
     }
   };
 
@@ -51,32 +48,39 @@ function App() {
       fetchAlerts();
       handleCloseModal();
     } catch (error) {
-      console.error('Error acknowledging alert:', error);
+      console.error("Error acknowledging alert:", error);
     }
   };
 
   const totalAlerts = alerts.length;
-  const acknowledgedAlerts = alerts.filter(alert => alert.isAcknowledged).length;
+  const acknowledgedAlerts = alerts.filter(
+    (alert) => alert.isAcknowledged
+  ).length;
 
   return (
     <div className="App">
-      <Navbar style={{ backgroundColor: '#4CAF50' }}>
-        <Navbar.Brand href="#" style={{ color: 'white', padding: 10 }}>Alerts Dashboard</Navbar.Brand>
+      <Navbar style={{ backgroundColor: "#4CAF50" }}>
+        <Navbar.Brand href="#" style={{ color: "white", padding: 10 }}>
+          Alerts Dashboard
+        </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text style={{ color: 'white' }}>
+          <Navbar.Text style={{ color: "white" }}>
             Total Alerts: {totalAlerts} | Acknowledged: {acknowledgedAlerts}
           </Navbar.Text>
         </Navbar.Collapse>
       </Navbar>
 
       <ListGroup className="m-3">
-        {alerts.map(alert => (
+        {alerts.map((alert) => (
           <ListGroup.Item
             key={alert._id}
             action
             onClick={() => handleOpenModal(alert)}
-            style={{ backgroundColor: alert.isAcknowledged ? '#d4edda' : '', cursor: 'pointer' }}
+            style={{
+              backgroundColor: alert.isAcknowledged ? "#d4edda" : "",
+              cursor: "pointer",
+            }}
           >
             {alert.ts} -- {alert.failure}
           </ListGroup.Item>
@@ -90,18 +94,34 @@ function App() {
         <Modal.Body>
           {selectedAlert && (
             <>
-              <p><strong>Failure Type:</strong> {selectedAlert.failure}</p>
-              <p><strong>Machine ID:</strong> {selectedAlert.machineID}</p>
-              <p><strong>Time:</strong> {selectedAlert.ts}</p>
-              <p><strong>Acknowledged:</strong> {selectedAlert.isAcknowledged ? "Yes" : "No"}</p>
-              <p><strong>Repair Steps:</strong> <pre>{selectedAlert.repairSteps}</pre></p>
-
+              <p>
+                <strong>Failure Type:</strong> {selectedAlert.failure}
+              </p>
+              <p>
+                <strong>Machine ID:</strong> {selectedAlert.machineID}
+              </p>
+              <p>
+                <strong>Time:</strong> {selectedAlert.ts}
+              </p>
+              <p>
+                <strong>Acknowledged:</strong>{" "}
+                {selectedAlert.isAcknowledged ? "Yes" : "No"}
+              </p>
+              <p>
+                <strong>Repair Steps:</strong>{" "}
+                <pre>{selectedAlert.repairSteps}</pre>
+              </p>
             </>
           )}
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
-          <Button variant="primary" onClick={() => acknowledgeAlert(selectedAlert._id)}>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => acknowledgeAlert(selectedAlert._id)}
+          >
             Acknowledge
           </Button>
         </Modal.Footer>
--- app/failure-prediction/utils/session-manager.js
+++
@@ -1,8 +0,0 @@
-import { v4 as uuidv4 } from 'uuid';
-
-let sessionId = uuidv4();
-
-export const getSessionId = () => sessionId;
-export const resetSessionId = () => {
-  sessionId = uuidv4();
-};
