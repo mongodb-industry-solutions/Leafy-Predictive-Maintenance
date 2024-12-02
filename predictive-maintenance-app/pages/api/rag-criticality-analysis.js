272abea4b31c68f5b571233e179880e2f2f61903
bereilhp <bereilhp@gmail.com>
2024-12-02 11:51:23 +0100

    rag criticality analysis api route with cohere and openai conf

--- predictive-maintenance-app/pages/api/rag-criticality-analysis.js
+++ predictive-maintenance-app/pages/api/rag-criticality-analysis.js
@@ -1,77 +1,104 @@
 import { MongoClient } from "mongodb";
+import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
+import { HumanMessage } from "@langchain/core/messages";
 import {
   BedrockRuntimeClient,
   InvokeModelCommand,
 } from "@aws-sdk/client-bedrock-runtime";
 import { BedrockChat } from "@langchain/community/chat_models/bedrock";
 
-const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
+const AI_MODEL_PROVIDER = process.env.AI_MODEL_PROVIDER;
+const uri = process.env.MONGODB_CONNECTION_STRING;
+const dbName = process.env.DATABASE;
+const collectionName = process.env.MAINTAINENCE_HISTORY_COLLECTION;
+const indexNameOpenAI = process.env.CRITICALITY_ANALYSIS_SEARCH_INDEX_OPEN_AI;
+const indexNameCohere = process.env.CRITICALITY_ANALYSIS_SEARCH_INDEX;
 
-const bedrockClient = new BedrockRuntimeClient({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
+const client = new MongoClient(uri);
 
-async function getEmbeddings(texts) {
-  const input = {
-    modelId: "cohere.embed-english-v3",
-    contentType: "application/json",
-    accept: "application/json",
-    body: JSON.stringify({
-      texts,
-      input_type: "search_document",
-    }),
+let model, embeddings, getEmbeddings, generateCompletion;
+
+if (AI_MODEL_PROVIDER === "openai") {
+  const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+  const OPENAI_API_MODEL = process.env.OPENAI_API_MODEL;
+  console.log("Using OpenAI");
+
+  model = new ChatOpenAI({
+    apiKey: OPENAI_API_KEY,
+    modelName: OPENAI_API_MODEL,
+  });
+
+  embeddings = new OpenAIEmbeddings({
+    apiKey: OPENAI_API_KEY,
+  });
+
+  getEmbeddings = async (texts) => {
+    return await embeddings.embedQuery(texts[0]);
   };
 
-  const command = new InvokeModelCommand(input);
-  const response = await bedrockClient.send(command);
-  const rawRes = response.body;
+  generateCompletion = async (prompt) => {
+    const response = await model.invoke([
+      new HumanMessage({ content: prompt }),
+    ]);
+    return response.content.trim();
+  };
+} else if (AI_MODEL_PROVIDER === "cohere") {
+  console.log("Using Cohere");
+  const bedrockClient = new BedrockRuntimeClient({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
 
-  const jsonString = new TextDecoder().decode(rawRes);
-  const parsedResponse = JSON.parse(jsonString);
+  getEmbeddings = async (texts) => {
+    const input = {
+      modelId: "cohere.embed-english-v3",
+      contentType: "application/json",
+      accept: "application/json",
+      body: JSON.stringify({
+        texts,
+        input_type: "search_document",
+      }),
+    };
 
-  return parsedResponse.embeddings;
-}
+    const command = new InvokeModelCommand(input);
+    const response = await bedrockClient.send(command);
+    const rawRes = response.body;
 
-const llm = new BedrockChat({
-  model: "cohere.command-r-v1:0",
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
+    const jsonString = new TextDecoder().decode(rawRes);
+    const parsedResponse = JSON.parse(jsonString);
 
-async function generateCompletion(prompt) {
-  try {
+    return parsedResponse.embeddings;
+  };
+
+  const llm = new BedrockChat({
+    model: "cohere.command-r-v1:0",
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
+  generateCompletion = async (prompt) => {
     const conversation = [
       ["system", "You are a helpful assistant."],
       ["human", prompt],
     ];
 
     const aiMessage = await llm.invoke(conversation);
-
-    const response = aiMessage.content.trim();
-    return response;
-  } catch (error) {
-    console.error("Error generating completion:", error);
-    throw new Error("Failed to generate completion");
-  }
+    return aiMessage.content.trim();
+  };
 }
 
-export default async function handler(
-  req = NextApiRequest,
-  res = NextApiResponse
-) {
+export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
 
   let { question, selectedDocuments } = req.body;
-
   selectedDocuments = selectedDocuments.map((i) => i + ".pdf");
 
   if (!question || !selectedDocuments) {
@@ -82,26 +109,29 @@ export default async function handler(
 
   try {
     await client.connect();
-    const db = client.db(process.env.DATABASE);
-    const collection = db.collection(
-      process.env.MAINTAINENCE_HISTORY_COLLECTION
-    );
-
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
     console.log("Connected to MongoDB");
 
     const vector = await getEmbeddings([question]);
+    console.log(vector);
 
     const filter = { "source.filename": { $in: selectedDocuments } };
+    const indexName =
+      AI_MODEL_PROVIDER === "openai" ? indexNameOpenAI : indexNameCohere;
 
     const results = await collection
       .aggregate([
         {
           $vectorSearch: {
-            index: process.env.CRITICALITY_ANALYSIS_SEARCH_INDEX,
-            path: "embeddings",
-            queryVector: vector[0],
+            index: indexName,
+            path:
+              AI_MODEL_PROVIDER === "openai"
+                ? "embeddings_openai"
+                : "embeddings",
+            queryVector: AI_MODEL_PROVIDER === "openai" ? vector : vector[0],
             numCandidates: 150,
-            limit: 15,
+            limit: 10,
             filter: filter,
           },
         },
@@ -113,9 +143,7 @@ export default async function handler(
 
     const prompt = `Given the following context sections, answer the question using only the given context. If you are unsure and the answer is not explicitly written in the documentation, say "Sorry, I don't know how to help with that as I cannot find this information in the docs you provided."\n\nContext:\n${context}\n\nQuestion: ${question}`;
 
-    const response = await generateCompletion(prompt);
-
-    const answer = response.trim();
+    const answer = await generateCompletion(prompt);
     res.status(200).json({ answer, dataSources });
   } catch (error) {
     console.error("Error in handler:", error);
